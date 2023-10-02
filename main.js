7230a9ac3addcf5c786422eaf429148f34a61ac3
lorenanfante <linfante@modyo.com>
2023-10-02 13:17:08 -0500

    adding daily weather section with all its functionalities from main and auxiliary functions mappers and getters

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
 
 function renderWeather({ current, daily, hourly }) {
     renderCurrentWeather(current);
-    //renderDailyWeather(daily);
+    renderDailyWeather(daily);
     //renderHourlyWeather(hourly);
     document.body.classList.remove('blurred');
 
@@ -38,4 +38,25 @@ function renderCurrentWeather(current) {
     setValue('current-fl-low', current.lowFeelsLike);
     setValue('current-wind', current.windSpeed);
     setValue('current-precip', current.precip);
+}
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: 'long' })
+const dailySection = document.querySelector('[data-day-section]');
+const dayCardTemplate = document.getElementById('day-card-template');
+
+function renderDailyWeather(daily) {
+    //currentIcon.src = getIconURL(daily.iconCode);
+    //let iconCode = document.querySelector('.day-card-date');
+    dailySection.innerHTML = '';
+    daily.forEach(day => {
+        const element = dayCardTemplate.content.cloneNode(true);
+        setValue('temp', day.maxTemp, { parent: element });
+        //console.log(`day.maxTemp: ${day.maxTemp}`);
+        setValue('date', DAY_FORMATTER.format(day.timestamp), { parent: element });
+        element.querySelector('[data-icon]').src = getIconURL(day.iconCode);
+        //console.log(`${element.querySelector('[data-icon]').src = getIconURL(day.iconCode)}`);
+        dailySection.append(element);
+
+        //console.log(`DAY_FORMATTER: ${DAY_FORMATTER.format(day.timestamp)}`);
+    })
+
 }
